90b72b323555d7ce05b2d134dcd275cb8992c4b8
rphsantos1995 <rph.santos@Outlook.com>
2022-02-10 17:13:17 -0300

    restoring test file

--- tests/req01-createUsers.test.js
+++ tests/req01-createUsers.test.js
@@ -128,7 +128,7 @@ describe('1 - Sua aplicação deve ter o endpoint POST `/user`', () => {
           password: '123456',
           image: 'https://www.globalframe.com.br/gf_base/empresas/MIGA/imagens/BDA23B2786FD3B7EC65745DC3FA1EE49D31B_barrichello-1.jpg',
         })
-      .expect('status', 409);
+      .expect('status', 201);
 
     await frisby
       .post(`${url}/user`,
